0c2eb2c06be12b6dbf22423dd5e5fc71e4ebf95a
A <pita2011@live.ca>
2026-03-04 15:32:59 -0500

    Added Practice Example

--- calculate_Area/calculate_Area.js
+++ calculate_Area/calculate_Area.js
@@ -6,6 +6,13 @@ function calculateArea() {
  width = parseFloat(document.getElementById('width').value);
 let area = length * width;
 document.getElementById('result').innerText = `The area of the rectangle is: ${area}`;
+}
 
+function calculateGrocery(){
+    let G1 = parseFloat(document.getElementById('G1').value);
+    let G2 = parseFloat(document.getElementById('G2').value);
+    let G3 = parseFloat(document.getElementById('G3').value);
+    let total = G1 + G2 + G3;
+    document.getElementById('Grocery_result').innerText = `The Grocery Total is: ${total}`;
 }
 
