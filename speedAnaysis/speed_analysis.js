01c53082ccd7325addadb8254841a01c71a974ca
= <=>
2026-03-05 15:25:07 -0500

    Added speed analysis practice example

--- speedAnaysis/speed_analysis.js
+++ speedAnaysis/speed_analysis.js
@@ -31,6 +31,7 @@ function endTest() {
     var typedWords = userTypedText.split(/\s+/).filter(function (word) {
         return word !== "";
     }).length;
+    var typedLength = userTypedText.length;
 
     var wpm = 0; // Default value
 
@@ -41,6 +42,7 @@ function endTest() {
     // Display the results
     var outputDiv = document.getElementById("output");
     outputDiv.innerHTML = "<h2>Typing Test Results:</h2>" +
+        "<p>Total Length: " + typedLength + "</p>" +
         "<p>Words Typed: " + typedWords + "</p>" +
         "<p>Time Elapsed: " + timeElapsed.toFixed(2) + " seconds</p>" +
         "<p>Words Per Minute (WPM): " + wpm + "</p>";
