9f84d6b7522fba96a51714e6259079357457d8b8
A <pita2011@live.ca>
2026-03-05 09:43:02 -0500

    Adding Practice Excercise Event Lab Files

--- productSurvey/product_survey.js
+++ productSurvey/product_survey.js
@@ -8,7 +8,7 @@ function submitFeedback() {
     const designation = document.getElementById('designation').value;
     const productType = document.getElementById('productType').value;
     const feedback = document.getElementById('feedbackText').value;
-    
+    const userExperience = document.getElementById("userExperience").value;
     alert('Thank you for your valuable feedback')
     document.getElementById('userName').innerHTML = username;
     document.getElementById('userAge').innerHTML = age;
@@ -16,13 +16,14 @@ function submitFeedback() {
     document.getElementById('userJob').innerHTML = job;
     document.getElementById('userDesignation').innerHTML = designation;
     document.getElementById('userProductChoice').innerHTML = productType;
+    document.getElementById('userUserExperience').innerHTML = userExperience;
     document.getElementById('userFeedback').innerHTML = feedback;
 
     document.getElementById('userInfo').style.display = 'block';
+}
 
+submitButton.onclick = submitFeedback;
 
-}submitButton.onclick = submitFeedback
-;
 document.addEventListener('keydown', function(event) {
     if (event.key === 'Enter') {
       submitFeedback();
